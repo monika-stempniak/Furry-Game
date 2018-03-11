d538f3eaa472635087eef3d0db4ac37e1b4d8756
monika <mstempniak@gmail.com>
2018-03-11 14:12:13 +0100

    add app.js and game func

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// modules
+var Game = require( "./game.js");
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  //variables
+  var btnPlay = document.querySelector('.btn-play');
+  var instruction = document.getElementById('instruction');
+  var btnInstruction = document.querySelector('.btn-instruction');
+  var btnClose = instruction.querySelector('.btn-close');
+  var btnOver = document.querySelector('.btn-over');
+
+  // display on board
+  var game = new Game();
+
+  game.showFurry();
+  game.showCoin();
+
+  // play game
+  btnPlay.addEventListener('click', function() {
+    game.startGame();
+    this.classList.add('play');
+    var child = this.querySelector('span');
+    child.innerText = '=';
+  });
+
+  // turn Furry
+  document.addEventListener('keydown', function(event) {
+    game.turnFurry(event);
+  });
+
+  // show game instruction
+  btnInstruction.addEventListener('click', function() {
+    instruction.classList.remove('invisible');
+  });
+
+  // close game instruction
+  btnClose.addEventListener('click', function() {
+    instruction.classList.add('invisible');
+  });
+
+  // play game again
+  btnOver.addEventListener('click', function() {
+    this.classList.add('btn-animate');
+    this.addEventListener('animationend', function() {
+      location.reload();
+    })
+  });
+
+
+});
